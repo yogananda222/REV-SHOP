9a3fe89f473b49fd5a3fe2d7f1b322e21db37f09
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-12 21:24:47 +0530

    users management service

---
+++ usersmanagementservice/src/main/java/com/revshop/usersmanagementservice/UsersmanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.revshop.usersmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersmanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersmanagementserviceApplication.class, args);
+	}
+
+}
---
+++ usersmanagementservice/src/test/java/com/revshop/usersmanagementservice/UsersmanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revshop.usersmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersmanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
