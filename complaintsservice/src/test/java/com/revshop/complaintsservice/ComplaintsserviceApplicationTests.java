403ce2d57ea3fb40c5821fd70c1795403b9dcede
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-12 21:22:23 +0530

    complaints service

---
+++ complaintsservice/src/test/java/com/revshop/complaintsservice/ComplaintsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revshop.complaintsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
