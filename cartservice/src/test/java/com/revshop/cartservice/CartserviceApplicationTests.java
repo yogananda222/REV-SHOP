798240edafa97c76096b06ef3c89c8945b5dcb27
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-12 21:09:16 +0530

    cart service

---
+++ cartservice/src/test/java/com/revshop/cartservice/CartserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revshop.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
