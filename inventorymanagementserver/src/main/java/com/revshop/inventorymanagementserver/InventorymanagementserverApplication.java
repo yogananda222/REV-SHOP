23a7dbfc47d0bae1fb81ddc5866c63020ce35a1e
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-12 21:22:58 +0530

    inventory management service

---
+++ inventorymanagementserver/src/main/java/com/revshop/inventorymanagementserver/InventorymanagementserverApplication.java
@@ -0,0 +1,13 @@
+package com.revshop.inventorymanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymanagementserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymanagementserverApplication.class, args);
+	}
+
+}
---
+++ inventorymanagementserver/src/test/java/com/revshop/inventorymanagementserver/InventorymanagementserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revshop.inventorymanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorymanagementserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
