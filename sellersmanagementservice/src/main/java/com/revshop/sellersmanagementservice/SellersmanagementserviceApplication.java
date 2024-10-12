97307831657c58ccc7c5271b75e5295f6c83013c
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-12 21:24:14 +0530

    sellers management service

---
+++ sellersmanagementservice/src/main/java/com/revshop/sellersmanagementservice/SellersmanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.revshop.sellersmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellersmanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellersmanagementserviceApplication.class, args);
+	}
+
+}
---
+++ sellersmanagementservice/src/test/java/com/revshop/sellersmanagementservice/SellersmanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revshop.sellersmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellersmanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
