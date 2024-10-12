1cc5916b80fb5cfc2e93a05ce65ee5a8dfe40c55
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-12 22:09:47 +0530

    product entity & enum category

---
+++ productservice/src/main/java/com/revshop/productservice/entity/Category.java
@@ -0,0 +1,23 @@
+package com.revshop.productservice.entity;
+
+public enum Category {
+	
+	TOPS ,
+	BOTTOMNS, 
+	FORMALWEAR,
+	WESTERN, 
+	WOMEN,
+	KIDS,
+	MEN,
+	UNDERGARMENTS,
+	NIGHTWEAR,
+	TRADITIONALWEAR,
+	KURTHAS,
+	TSHIRTS,
+	JEANS
+	
+	
+	
+	
+
+}
