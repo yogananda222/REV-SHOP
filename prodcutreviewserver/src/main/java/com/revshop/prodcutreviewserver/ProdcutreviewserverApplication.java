11a5614cbc88cbaaf475c1a9157f5f55912a12b4
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-12 21:23:40 +0530

    product reviews management service

---
+++ prodcutreviewserver/src/main/java/com/revshop/prodcutreviewserver/ProdcutreviewserverApplication.java
@@ -0,0 +1,13 @@
+package com.revshop.prodcutreviewserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdcutreviewserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdcutreviewserverApplication.class, args);
+	}
+
+}
---
+++ prodcutreviewserver/src/test/java/com/revshop/prodcutreviewserver/ProdcutreviewserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revshop.prodcutreviewserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdcutreviewserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
