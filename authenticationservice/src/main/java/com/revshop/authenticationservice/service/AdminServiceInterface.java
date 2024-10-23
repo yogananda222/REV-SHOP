344899d30b834369980f580e2467301fb0952f78
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-23 23:37:49 +0530

    admin service interface

---
+++ authenticationservice/src/main/java/com/revshop/authenticationservice/service/AdminServiceInterface.java
@@ -0,0 +1,10 @@
+package com.revshop.authenticationservice.service;
+
+import com.revshop.authenticationservice.entity.Admin;
+
+public interface AdminServiceInterface {
+	
+	
+	Admin adminLogin(Admin admin);
+
+}
