555061758207017faa1d7bf84ac6ec8c11108dda
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-23 23:24:12 +0530

    Invalid credentials exception

---
+++ authenticationservice/src/main/java/com/revshop/authenticationservice/exception/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package com.revshop.authenticationservice.exception;
+
+
+public class InvalidCredentialsException extends RuntimeException {
+	
+	
+	public InvalidCredentialsException(String message) {
+		super(message);
+	}
+
+}
